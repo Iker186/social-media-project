7cf0a0926e2aa13aa908cd2f431c3bce3f9e717a
Iker186 <sancheziker549@gmail.com>
2025-04-07 18:36:19 -0600

    streamlit y workflow 1

--- app.py
+++ app.py
@@ -42,43 +42,8 @@ with col1:
 
 with col2:
     github_token = st.text_input('🔑 Github token', value='', type="password")  
-    code_url     = st.text_input('🧾 Code URL', value='https://raw.githubusercontent.com/Iker186/analisis_datos/main/spark_process.py')
-    dataset_url  = st.text_input('📊 Dataset URL', value='https://raw.githubusercontent.com/Iker186/analisis_datos/main/data/social_media.csv')
+    code_url     = st.text_input('🧾 Code URL', value='https://raw.githubusercontent.com/Iker186/social-media-project/main/spark_process.py')
+    dataset_url  = st.text_input('📊 Dataset URL', value='https://raw.githubusercontent.com/Iker186/social-media-project/main/data/social_media.csv')
 
 if st.button("▶️ POST spark submit"):
    post_spark_job(github_user, github_repo, spark_job, github_token, code_url, dataset_url)
-
-st.markdown("---")
-st.subheader("⚙️ Disparar otros workflows")
-
-col3, col4 = st.columns(2)
-
-with col3:
-    if st.button("🐳 POST Mongo workflow"):
-        post_spark_job(github_user, github_repo, "migrate_mongo", github_token, code_url, dataset_url)
-
-with col4:
-    if st.button("🐘 POST PostgreSQL workflow"):
-        post_spark_job(github_user, github_repo, "migrate_postgres", github_token, code_url, dataset_url)
-
-st.markdown("---")
-st.subheader("📥 Obtener resultados")
-
-def get_spark_results(url_results):
-    if not url_results:
-        st.error("❌ URL de resultados no proporcionada.")
-        return
-    
-    response = requests.get(url_results)
-    
-    if response.status_code == 200:
-        try:
-            st.write(response.json())
-        except ValueError:
-            st.write(response.text)
-    else:
-        st.error(f"❌ Error al obtener resultados: {response.status_code}")
-
-url_resultados = st.text_input("📄 URL de resultados (JSON o texto)")
-if st.button("📡 Cargar resultados"):
-    get_spark_results(url_resultados)
